da390cd3730e26061f2ee4c8bb6d5fc4ea7fad87
mohammed-ameen <menooteaching10@gmail.com>
2022-01-03 04:34:37 +0200

    src files

---
+++ src/client/index.js
@@ -0,0 +1,6 @@
+import ("./public/style/style.css");
+import ("./public/js/app.js");
+import ("./public/js/carousel.js");
+import ("./public/js/form.js");
+import ("./public/js/jqueryMod.js");
+import ("./public/js/typeIt.js");
---
+++ src/client/public/js/jqueryMod.js
@@ -0,0 +1,26 @@
+
+$(function($){
+        let arrowUp = document.querySelector(".arrow-up");
+        let arrowDown = document.querySelector(".arrow-down");
+        let car = document.querySelector(".carousel-images");
+        let cerImg = document.querySelectorAll(".cert-img img");
+        let cerImagesDiv = document.querySelector(".cert-images");
+        let bigImg = document.querySelector(".big-img");
+
+        $(".cert-images").on("click",function (e) {
+           let image =  e.target;
+           let mainImgSrc = $(image).attr("src");
+          $(".cert-img img").each(function(i){
+              $(this).removeClass("active");
+          });
+          $(image).addClass("active");
+          $(bigImg).addClass("active");
+
+          $(bigImg).hide(200, function(){
+            $(bigImg).attr("src", mainImgSrc );
+            $(bigImg).show(10);
+        });
+         
+          
+        });
+});
