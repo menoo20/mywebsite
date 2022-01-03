192cd098a514d115f2fa3a5b1f4ff972da5a8e75
mohammed-ameen <menooteaching10@gmail.com>
2022-01-03 04:38:56 +0200

    Updates

---
+++ src_client_public_js_form_js.bundle.js
@@ -0,0 +1,3 @@
+/*! For license information please see src_client_public_js_form_js.bundle.js.LICENSE.txt */
+(self.webpackChunkmyportfolio3=self.webpackChunkmyportfolio3||[]).push([["src_client_public_js_form_js"],{"./src/client/public/js/form.js":()=>{document.querySelector("#contact-form").addEventListener("submit",(e=>{e.preventDefault(),e.target.elements.name.value="",e.target.elements.email.value="",e.target.elements.message.value="",console.log(e.target.elements.name.value)}))}}]);
+//# sourceMappingURL=src_client_public_js_form_js.bundle.js.map
---
+++ src_client_public_style_style_css.bundle.js
@@ -0,0 +1,2 @@
+/*! For license information please see src_client_public_style_style_css.bundle.js.LICENSE.txt */
+"use strict";(self.webpackChunkmyportfolio3=self.webpackChunkmyportfolio3||[]).push([["src_client_public_style_style_css"],{"./src/client/public/style/style.css":(s,l,c)=>{c.r(l)}}]);
